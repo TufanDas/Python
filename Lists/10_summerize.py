64a6caee19cf9cc031ad2e9da21cd937cdb1bc56
Tufan <dtufan38@gmail.com>
2025-06-25 10:30:18 +0530

    summerize previous 1-10 filse in Lists

---
+++ Lists/10_summerize.py
@@ -0,0 +1,38 @@
+"""
+List are ordered mutable collections that can store mixed datatypes
+
+use [] or the list() constructor to craete a list
+
+use indexing
+    my_list[0],
+    my_ist[-1] and
+
+slicing
+    my_list[1:4] to retrive data
+
+change values via index assignment
+
+Add with append(),insert(),exted()
+
+Remove with remove(), pop(), del or clear()
+
+use slicing ([:]), list() or copy()
+
+shallow and deep copies
+
+combine with +
+repeat with *
+check membership with in
+
+use len(), min(), max(), sum() for quick data insights
+
+sort with sort() / sorted()
+reverse with reverse() / reversed()
+
+find data with index() and count with count()
+
+Lists can containts other list (2D structures)
+
+== checks values, is checkes memory reference
+
+"""
