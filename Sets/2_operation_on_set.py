f2c15573e4db6aa17161d1df2fc076e7c5165e99
Tufan <dtufan38@gmail.com>
2025-07-05 19:52:12 +0530

    operations on sets

---
+++ Sets/2_operation_on_set.py
@@ -0,0 +1,25 @@
+"""
+what is sets
+# unordered, unindexed, mutable, unique, hetrerogeneous
+should have immutable items
+
+# union, intersaction, difference
+# fast membership testing
+
+operations:
+union: inclued all
+intersection : common elements
+a - b = uncommon elements are remains
+symetric difference: both set containts uncommon elements
+
+
+
+
+
+why sets is very important?
+creation of a set
+operations can perform sets
+mathometical operations on set
+practical examples
+frozen sets
+"""
